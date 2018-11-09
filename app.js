f72060ad594d97059361330d902151a9b9d70654
Carolyn <ccpianist@gmail.com>
2018-11-09 11:30:16 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const morgan = require("morgan");
+const express = require("express");
+const htmlTemplateTag = require("html-template-tag");
+const layout = require("./views/layout");
+const PORT = 1337;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send(layout(''));
+});
